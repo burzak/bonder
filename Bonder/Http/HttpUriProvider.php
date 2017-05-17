965166cc71cf0aad2cb0fc79310582f1d4cc5693
Dario Ocles <dario.ocles@gmail.com>
2017-05-17 11:24:33 -0300

    DOCUMENT_URI or REQUEST_URI

--- Bonder/Http/HttpUriProvider.php
+++ Bonder/Http/HttpUriProvider.php
@@ -10,6 +10,9 @@ namespace Bonder\Http;
 final class HttpUriProvider implements \Bonder\UriProvider {
   
   public function getUri() {
+    if (empty($_SERVER["DOCUMENT_URI"])) {
+      return isset($_SERVER["REQUEST_URI"])?$_SERVER["REQUEST_URI"]:null;
+    }
     return isset($_SERVER["DOCUMENT_URI"])?$_SERVER["DOCUMENT_URI"]:null;
   }
   
